bc15a66822fd0ffd967978d360e55d39652ddc4b
lddg0502 <ldomingos@correo.unimet.edu.ve>
2023-03-11 07:08:22 -0400

    Login

--- mp2/src/pages/LoginPage/LoginPage.jsx
+++ mp2/src/pages/LoginPage/LoginPage.jsx
@@ -1,7 +1,20 @@
+import './LoginPage.css'
+
 export function LoginPage(){
     return(
-        <>
-        <h1>Soy Login</h1>
-        </>
+        <div className='fondo'>
+          <form>
+            <h1 className='titulo'>Login</h1>
+            <label>
+              <input placeholder='Usuario' className='username'></input>
+            </label>
+            <label>
+              <input type={'password'} placeholder='Password' className='password'></input>
+            </label>
+
+            <button className='button-login'>Login</button>
+            <button className='button-google'>With google</button>
+          </form>
+        </div>
     )
 }
