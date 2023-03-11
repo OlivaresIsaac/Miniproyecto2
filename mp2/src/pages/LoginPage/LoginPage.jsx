248467f3f9e2b01ff89ae29d01db902baf22034a
isaacBakugan <97451474+OlivaresIsaac@users.noreply.github.com>
2023-03-11 09:55:05 -0400

    implement register and login form and val

--- mp2/src/pages/LoginPage/LoginPage.jsx
+++ mp2/src/pages/LoginPage/LoginPage.jsx
@@ -1,19 +1,59 @@
 import './LoginPage.css'
+import { loginWithEmailAndPassword, signInWithGoogle } from "../../firebase/auth-service"
+import { LANDING_URL } from "../../constants/url"
+import { REGISTER_URL } from "../../constants/url"
+import { useState } from "react"
+import { useNavigate } from "react-router"
 
 export function LoginPage(){
+
+  const handleLoginWithGoogle = async () => {
+    await signInWithGoogle().then( (result) => {
+      if(result) {
+        navigate(LANDING_URL)
+      }
+    })
+ }
+
+ const navigate = useNavigate()
+    const [formData, setFormData] = useState({
+        email: "",
+        password: ""
+    })
+
+    const handleOnChange = (event) => {
+        const {name, value} = event.target
+        setFormData({
+            ...formData,
+            [name]: value
+        })
+    }
+
+
+ const handleOnSubmit = async (event) => {
+  event.preventDefault()
+  const {email, password} = formData
+
+  await loginWithEmailAndPassword(email, password).then((result) => {
+    if (result === true) {
+      navigate(LANDING_URL)
+    }
+  });
+ }
+
     return(
         <div className='fondo'>
-          <form>
+          <form onSubmit={handleOnSubmit}>
             <h1 className='titulo'>Login</h1>
             <label>
-              <input placeholder='Usuario' className='username'></input>
+              <input placeholder='Email' className='username' name='email' onChange={handleOnChange}></input>
             </label>
             <label>
-              <input type={'password'} placeholder='Password' className='password'></input>
+              <input type={'password'} placeholder='Password' className='password' name='password' onChange={handleOnChange}></input>
             </label>
 
-            <button className='button-login'>Login</button>
-            <button className='button-google'>With google</button>
+            <button className='button-login' type='submit'>Login</button>
+            <button className='button-google' onClick={handleLoginWithGoogle}>With google</button>
           </form>
         </div>
     )
